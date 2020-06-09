5f896a36af5d42cb69178d6d97ace99de794dd32
Chase Schelthoff <schelthoff.chase@gmail.com>
2020-06-09 18:43:48 -0400

    start parsing messages

--- src/bot.py
+++ src/bot.py
@@ -5,16 +5,46 @@ import config
 
 client = discord.Client()
 
+def parse_code_message(message):
+    '''
+    extracts the language & code from a message.
+
+    example:
+    ```python
+    print("Hello, world")
+    ```
+    '''
+    split_message = message.content.split('\n')
+
+    language = split_message[0][3:].strip()
+    code = '\n'.join(split_message[1:-1])
+
+    return language, code
+
 def should_ignore_message(message):
+    channel = message.channel
+    content = message.content
+
+    is_code_message = content.startswith('```') and content.endswith('```') 
     return any([message.author == client.user,
-                message.channel.id not in config.get('allowed_channels')])
+                channel.id not in config.get('allowed_channels'),
+                not is_code_message])
+
+async def send_message(channel, msg):
+    # TODO: chunk the message into 2000 characters each
+    return await channel.send(msg[:2000])
 
 @client.event
 async def on_message(message):
     if should_ignore_message(message):
         return
 
+    channel = message.channel
+    
     print(message.content, 'in', message.channel)
+    language, code = parse_code_message(message)
+
+    await send_message(channel, 'language: {0}\ncode: {1}'.format(language, code))
 
 if __name__ == '__main__':
     print(discord.__version__)
